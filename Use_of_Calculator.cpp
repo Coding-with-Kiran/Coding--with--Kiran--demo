58e1af3a560696f0ec80d1b2c3bef24fac157972
Coding-with-Kiran <kiransafdarali503@gmail.com>
2025-02-24 17:50:23 +0500

    Add files via upload

---
+++ Use_of_Calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a ;
+		int b ;
+	cout << "enter a nmb";
+	cin >> a;
+	cout << "entr an other ";
+	cin >> b;
+	cout << "the addition of the nmbers is " << a + b << endl;
+	cout << "the mull of two nmbers is " << a * b << endl;
+	cout << "the subtraction of two nmbers is" << a-b << endl;
+	cout << " the division of two nmbers is " << a / b << endl;
+	return 0;
+}
